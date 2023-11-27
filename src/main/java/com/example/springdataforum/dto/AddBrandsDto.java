c7cf9fe88be240d6a9fa6c49f814605041e3646f
russi <russianthief3@gmail.com>
2023-11-27 11:58:05 +0300

    add bootstrap.min.css and html's

--- src/main/java/com/example/springdataforum/dto/AddBrandsDto.java
+++ src/main/java/com/example/springdataforum/dto/AddBrandsDto.java
@@ -1,4 +1,105 @@
 package com.example.springdataforum.dto;
 
+import com.example.springdataforum.Constans.CategoryOfVehicles;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class AddBrandsDto {
+    UUID id;
+    String name;
+    CategoryOfVehicles category;
+    String imageURL;
+    int startYear;
+    int endYear;
+    LocalDateTime created;
+    LocalDateTime modified;
+    ShowDetailedBrandsInfoDto brand;
+
+    public AddBrandsDto(UUID id, String name, CategoryOfVehicles category, String imageURL, int startYear, int endYear, LocalDateTime created, LocalDateTime modified, ShowDetailedBrandsInfoDto brand) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.imageURL = imageURL;
+        this.startYear = startYear;
+        this.endYear = endYear;
+        this.created = created;
+        this.modified = modified;
+        this.brand = brand;
+    }
+
+    protected AddBrandsDto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public ShowDetailedBrandsInfoDto getBrand() {
+        return brand;
+    }
+
+    public void setBrand(ShowDetailedBrandsInfoDto brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryOfVehicles getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryOfVehicles category) {
+        this.category = category;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(int endYear) {
+        this.endYear = endYear;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
 }
