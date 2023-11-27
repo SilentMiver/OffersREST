2c7e93f87adeadd1ceab6a15644b7e9dbf6b1d7c
russi <russianthief3@gmail.com>
2023-11-27 11:22:32 +0300

    change in repository

--- src/main/java/com/example/springdataforum/repositories/BrandsRepository.java
+++ src/main/java/com/example/springdataforum/repositories/BrandsRepository.java
@@ -2,8 +2,13 @@ package com.example.springdataforum.repositories;
 
 import com.example.springdataforum.models.Brands;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface BrandsRepository extends JpaRepository<Brands, UUID> {
+    @Modifying
+    @Transactional
+    void deleteByName(String name);
 }
--- src/main/java/com/example/springdataforum/repositories/ModelsRepository.java
+++ src/main/java/com/example/springdataforum/repositories/ModelsRepository.java
@@ -2,8 +2,13 @@ package com.example.springdataforum.repositories;
 
 import com.example.springdataforum.models.Models;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface ModelsRepository extends JpaRepository<Models, UUID> {
+    @Modifying
+    @Transactional
+    void deleteByName(String name);
 }
